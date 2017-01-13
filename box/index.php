636fbbb0ffb2391ec3247c88b0adbc1aa7602f8b
sarderiftekhar <sarder2008@gmail.com>
2017-01-13 00:17:11 +0000

    Font fomily changed
    Font of footer changed to Arial

---
+++ box/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Basic Shout Box</title>
+	<link rel="stylesheet" href="stylesheet.css">
+
+</head>
+<body>
+
+<div class="wrapper clr">
+	
+	<header class="headsection clr">
+			
+			<h2>Basic Shout Box with PHP OOP and MySqli</h2>
+
+	</header><!--End of headsection clr -->
+	
+	<sectionc class="content clr">
+
+		<div class="box">
+			<ul>
+				<li><span>-<b>Sarder</b> Hello how are you</span></li>
+				<li><span>-<b>Sarder</b> Hello how are you</span></li>
+				<li><span>-<b>Sarder</b> Hello how are you</span></li>
+				<li><span>-<b>Sarder</b> Hello how are you</span></li>
+				<li><span>-<b>Sarder</b> Hello how are you</span></li>
+				<li><span>-<b>Sarder</b> Hello how are you</span></li>
+				<li><span>-<b>Sarder</b> Hello how are you</span></li>
+			</ul>
+
+		</div><!-- End of Box -->
+
+
+	<div class="shoutform clr">
+		
+				<form action="POST">
+					
+					<table>
+							<tr>
+								<td>Name</td>
+								<td>:</td>
+								<td><input type="text" name="name" required/></td>
+							</tr>
+							
+							<tr>
+								<td>Body</td>
+								<td>:</td>
+								<td><input type="text" name="name" required/></td>
+							</tr>
+
+							<tr>
+								<td></td>
+								<td></td>
+								<td><input type="submit" value="Shout it" /></td>
+							</tr>
+							
+					</table>
+
+				</form>
+
+
+
+	</div><!-- End of shoutform -->
+
+
+
+	</section><!-- end of content clr -->
+
+	<footer class="footsection clr">
+	
+		<h2>&copy: Copyright Sarder Iftekhar Jan | 2017</h2>
+	
+	</footer>
+
+</div> <!-- End of Wrapper -->
+</body>
+</html>
